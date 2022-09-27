a793d2fe314b4476a65d5f84ba08b3664c059910
Lil <liliapaper@gmail.com>
2022-09-27 20:13:00 +0300

    Moved users data into new class TestData

--- src/test/java/com/liliapaper/tests/AutomationPracticeFormTest.java
+++ src/test/java/com/liliapaper/tests/AutomationPracticeFormTest.java
@@ -1,32 +1,12 @@
 package com.liliapaper.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
 import com.liliapaper.pages.RegistrationFormPage;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-public class AutomationPracticeFormTest {
+public class AutomationPracticeFormTest extends TestData {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-    Faker faker = new Faker();
-    File file = new File("src/test/resources/Photo.png");
-    String name = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String userNumber = RandomStringUtils.randomNumeric(10, 10);
-    String subjects = "Hindi";
-    String currentAddress = faker.address().streetAddress();
-    String userMail = faker.internet().emailAddress();
-    String gender = "Female";
-    String month = "January";
-    String year = "2000";
-    String day = "10";
-    String state = "Uttar Pradesh";
-    String city = "Agra";
-    String hostname = "/automation-practice-form";
-    String hobb = "Sports";
 
     @BeforeAll
     public static void setUp() {
